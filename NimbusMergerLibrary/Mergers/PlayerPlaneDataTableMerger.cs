1f505559ac553cf4bad6c4311fc651d56984de01
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2024-12-07 19:15:44 +0100

    Removed comments

--- NimbusMergerLibrary/Mergers/PlayerPlaneDataTableMerger.cs
+++ NimbusMergerLibrary/Mergers/PlayerPlaneDataTableMerger.cs
@@ -55,18 +55,11 @@ namespace NimbusMergerLibrary.Mergers
             UDataTable gameTable = dataTable.Table;
             List<StructPropertyData> gameDatas = gameTable.Data;
 
+            _rowForCopy = gameDatas[0];
+
             // Initialize the dictionary
             foreach (StructPropertyData data in gameDatas)
             {
-                /*using (StreamWriter sw = File.CreateText("E:\\MODDING\\_Ace Combat 7\\_testForMerger\\~~~~~~~MERGER_TEST_P\\test.txt"))
-                {
-                    foreach (var line in data.Value)
-                    {
-                        sw.WriteLine($"outputRow[\"{line.Name}\"] = copiedRow[\"{line.Name}\"];");
-                    }
-                }*/
-
-
                 // Add the game plane IDs
                 IntPropertyData planeId = (IntPropertyData)data["PlaneID"];
                 _gamePlaneIDs.Add(planeId.Value);
@@ -84,8 +77,6 @@ namespace NimbusMergerLibrary.Mergers
                 string planeString = GetPlaneString(planeStringID, cmn, dat);
                 _planeStringsDict.Add(planeId.Value, planeString);
             }
-
-            _rowForCopy = gameDatas[0];
 
             _exportSortNumbers.Add(41);
             _exportPlaneIDs = new List<int>(_gamePlaneIDs);
@@ -117,123 +108,6 @@ namespace NimbusMergerLibrary.Mergers
                         break;
                 }
             }
-
-            /*outputRow["PlaneID"] = copiedRow["PlaneID"];
-            outputRow["OriginalPlaneID"] = copiedRow["OriginalPlaneID"];
-            outputRow["PlaneStringID"] = copiedRow["PlaneStringID"];
-
-            outputRow["TargetMode"] = copiedRow["TargetMode"];
-            DataTableUtils.FixPropertyReference((EnumPropertyData)outputRow["TargetMode"], _gameAsset);
-            
-            outputRow["Reference"] = copiedRow["Reference"];
-
-            outputRow["Category"] = copiedRow["Category"];
-            DataTableUtils.FixPropertyReference((EnumPropertyData)outputRow["Category"], _gameAsset);
-            outputRow["HangarSize"] = copiedRow["HangarSize"];
-            DataTableUtils.FixPropertyReference((EnumPropertyData)outputRow["HangarSize"], _gameAsset);
-            outputRow["IGCSize"] = copiedRow["IGCSize"];
-            DataTableUtils.FixPropertyReference((EnumPropertyData)outputRow["IGCSize"], _gameAsset);
-            outputRow["GunCaliber"] = copiedRow["GunCaliber"];
-            DataTableUtils.FixPropertyReference((EnumPropertyData)outputRow["GunCaliber"], _gameAsset);
-
-            outputRow["GunLoadCount"] = copiedRow["GunLoadCount"];
-            outputRow["MainWeaponLoadCount"] = copiedRow["MainWeaponLoadCount"];
-            outputRow["SpWeaponID1"] = copiedRow["SpWeaponID1"];
-            outputRow["SpWeaponLoadCount1"] = copiedRow["SpWeaponLoadCount1"];
-            outputRow["SpWeaponID2"] = copiedRow["SpWeaponID2"];
-            outputRow["SpWeaponLoadCount2"] = copiedRow["SpWeaponLoadCount2"];
-            outputRow["SpWeaponID3"] = copiedRow["SpWeaponID3"];
-            outputRow["SpWeaponLoadCount3"] = copiedRow["SpWeaponLoadCount3"];
-            outputRow["FlareLoadCount"] = copiedRow["FlareLoadCount"];
-            outputRow["FlareEffectiveRange_MP"] = copiedRow["FlareEffectiveRange_MP"];
-
-            outputRow["HangarAcquireCamera"] = copiedRow["HangarAcquireCamera"];
-            DataTableUtils.FixPropertyReference((StructPropertyData)outputRow["HangarAcquireCamera"], _gameAsset);
-
-            outputRow["SortNumber"] = copiedRow["SortNumber"];
-            outputRow["AlphabeticalSortNumber"] = copiedRow["AlphabeticalSortNumber"];
-
-            outputRow["GraphAirToAir"] = copiedRow["GraphAirToAir"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["GraphAirToAir"], _gameAsset);
-            outputRow["GraphAirToGround"] = copiedRow["GraphAirToGround"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["GraphAirToGround"], _gameAsset);
-            outputRow["GraphSpeed"] = copiedRow["GraphSpeed"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["GraphSpeed"], _gameAsset);
-            outputRow["GraphMobirity"] = copiedRow["GraphMobirity"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["GraphMobirity"], _gameAsset);
-            outputRow["GraphStability"] = copiedRow["GraphStability"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["GraphStability"], _gameAsset);
-            outputRow["GraphDefense"] = copiedRow["GraphDefense"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["GraphDefense"], _gameAsset);
-            outputRow["PartsSlotBody"] = copiedRow["PartsSlotBody"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["PartsSlotBody"], _gameAsset);
-            outputRow["PartsSlotArms"] = copiedRow["PartsSlotArms"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["PartsSlotArms"], _gameAsset);
-            outputRow["PartsSlotMisc"] = copiedRow["PartsSlotMisc"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["PartsSlotMisc"], _gameAsset);
-            outputRow["StealthLevel"] = copiedRow["StealthLevel"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["StealthLevel"], _gameAsset);
-            outputRow["MaxHealth"] = copiedRow["MaxHealth"];
-            DataTableUtils.FixPropertyReference((BytePropertyData)outputRow["MaxHealth"], _gameAsset);
-
-            outputRow["AircraftCost"] = copiedRow["AircraftCost"];
-            outputRow["CanopyEffectChangeDuration_MP"] = copiedRow["CanopyEffectChangeDuration_MP"];
-            outputRow["CanopyIceAppearDuration_MP"] = copiedRow["CanopyIceAppearDuration_MP"];
-            outputRow["CanopyIceChangeDuration_MP"] = copiedRow["CanopyIceChangeDuration_MP"];
-            outputRow["CanopyEffectDecayMultiplier_MP"] = copiedRow["CanopyEffectDecayMultiplier_MP"];
-            outputRow["IceTriggerCloudDensity_MP"] = copiedRow["IceTriggerCloudDensity_MP"];
-            outputRow["AirCurrentPositionInfluenceModifier_MP"] = copiedRow["AirCurrentPositionInfluenceModifier_MP"];
-            outputRow["AirCurrentRotationInfluenceModifier_MP"] = copiedRow["AirCurrentRotationInfluenceModifier_MP"];
-            outputRow["InAirCurrentMovementDegradePercent_MP"] = copiedRow["InAirCurrentMovementDegradePercent_MP"];
-            outputRow["AirCurrentStrengthThreshold_MP"] = copiedRow["AirCurrentStrengthThreshold_MP"];
-            outputRow["InCloudMovementDegradePercent_MP"] = copiedRow["InCloudMovementDegradePercent_MP"];
-            outputRow["InCloudMovementDegradeDuration_MP"] = copiedRow["InCloudMovementDegradeDuration_MP"];
-            outputRow["InCloudStallSpeed_MP"] = copiedRow["InCloudStallSpeed_MP"];
-            outputRow["InCloudBuffetSpeed_MP"] = copiedRow["InCloudBuffetSpeed_MP"];
-            outputRow["CloudEnterDensity_MP"] = copiedRow["CloudEnterDensity_MP"];
-            outputRow["CloudExitDensity_MP"] = copiedRow["CloudExitDensity_MP"];
-
-            outputRow["RefPlaneImagePortrait"] = copiedRow["RefPlaneImagePortrait"];
-            DataTableUtils.FixPropertyReference((StructPropertyData)outputRow["RefPlaneImagePortrait"], _gameAsset);
-
-            outputRow["RefPlaneImageLandscape"] = copiedRow["RefPlaneImageLandscape"];
-            DataTableUtils.FixPropertyReference((StructPropertyData)outputRow["RefPlaneImageLandscape"], _gameAsset);
-
-            outputRow["BeginAerialRefuelling"] = copiedRow["BeginAerialRefuelling"];
-            outputRow["BeginAerialRefuelling"] = copiedRow["BeginAerialRefuelling"];
-            outputRow["BeginAerialRefuelling"] = copiedRow["BeginAerialRefuelling"];
-            outputRow["BeginLanding"] = copiedRow["BeginLanding"];
-            outputRow["BeginLanding"] = copiedRow["BeginLanding"];
-            outputRow["BeginLanding"] = copiedRow["BeginLanding"];
-            outputRow["BeginTakeoffA"] = copiedRow["BeginTakeoffA"];
-            outputRow["BeginTakeoffA"] = copiedRow["BeginTakeoffA"];
-            outputRow["BeginTakeoffA"] = copiedRow["BeginTakeoffA"];
-            outputRow["BeginTakeoffB"] = copiedRow["BeginTakeoffB"];
-            outputRow["BeginTakeoffB"] = copiedRow["BeginTakeoffB"];
-            outputRow["BeginTakeoffB"] = copiedRow["BeginTakeoffB"];
-            outputRow["BeginTakeoffC"] = copiedRow["BeginTakeoffC"];
-            outputRow["BeginTakeoffC"] = copiedRow["BeginTakeoffC"];
-            outputRow["BeginTakeoffC"] = copiedRow["BeginTakeoffC"];
-            outputRow["BeginTakeoffNavyA"] = copiedRow["BeginTakeoffNavyA"];
-            outputRow["BeginTakeoffNavyA"] = copiedRow["BeginTakeoffNavyA"];
-            outputRow["BeginTakeoffNavyA"] = copiedRow["BeginTakeoffNavyA"];
-            outputRow["BeginTakeoffNavyB"] = copiedRow["BeginTakeoffNavyB"];
-            outputRow["BeginTakeoffNavyB"] = copiedRow["BeginTakeoffNavyB"];
-            outputRow["BeginTakeoffNavyB"] = copiedRow["BeginTakeoffNavyB"];
-            outputRow["BeginTakeoffNavyC"] = copiedRow["BeginTakeoffNavyC"];
-            outputRow["BeginTakeoffNavyC"] = copiedRow["BeginTakeoffNavyC"];
-            outputRow["BeginTakeoffNavyC"] = copiedRow["BeginTakeoffNavyC"];
-            outputRow["EndAerialRefuelling"] = copiedRow["EndAerialRefuelling"];
-            outputRow["EndAerialRefuelling"] = copiedRow["EndAerialRefuelling"];
-            outputRow["EndAerialRefuelling"] = copiedRow["EndAerialRefuelling"];
-            outputRow["EndTakeOff"] = copiedRow["EndTakeOff"];
-            outputRow["EndTakeOff"] = copiedRow["EndTakeOff"];
-            outputRow["EndTakeOff"] = copiedRow["EndTakeOff"];
-            outputRow["WeaponChange"] = copiedRow["WeaponChange"];
-            outputRow["WeaponChange"] = copiedRow["WeaponChange"];
-            outputRow["WeaponChange"] = copiedRow["WeaponChange"];
-            outputRow["IsUnlockAircraftTree"] = copiedRow["IsUnlockAircraftTree"];
-            outputRow["DLCID"] = copiedRow["DLCID"];*/
         }
 
         private void Sort()
@@ -295,12 +169,12 @@ namespace NimbusMergerLibrary.Mergers
             sortNumber.Value = _addSortNumber;
             _exportSortNumbers.Add(sortNumber.Value);
 
-            // Fix SoftObjectProperty index for the name map
+            // Add the new asseth path name to the name map
             string assetPathName = reference.Value.AssetPath.AssetName.ToString();
-            reference.Value = new FSoftObjectPath(new FTopLevelAssetPath(null, new FName(_gameAsset, assetPathName)), null); // Add the reference to the name map
-
-            modData.Name.Number = planeId.Value + 1; // Change row name
+            // Fix the reference to point to the asseth path name
+            reference.Value = new FSoftObjectPath(new FTopLevelAssetPath(null, new FName(_gameAsset, assetPathName)), null); 
             
+            // Copy the first row of the game asset
             StructPropertyData outputRow = (StructPropertyData)_rowForCopy.Clone();
             CopyRow(modData, outputRow);
             outputRow.Name.Number = planeId.Value + 1; // Change row name
