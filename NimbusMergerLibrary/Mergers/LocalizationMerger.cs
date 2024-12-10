6235e55f83c5f7005cced277e3bc35d97a0165ad
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2024-12-10 19:35:59 +0100

    Fixed missing string

--- NimbusMergerLibrary/Mergers/LocalizationMerger.cs
+++ NimbusMergerLibrary/Mergers/LocalizationMerger.cs
@@ -12,21 +12,31 @@ namespace NimbusMergerLibrary.Mergers
     {
         public static void MergeCmn(CmnFile exportCmn, CmnFile modCmn, Dictionary<char, DatFile> exportDats, Dictionary<char, DatFile> modDats, int gameMaxStringNumber, SortedDictionary<string, CmnString> parent)
         {
+            // Iterate through all the strings of the Cmn
             foreach (string key in parent.Keys)
             {
+                // If it's a new variable
                 if (gameMaxStringNumber < parent[key].StringNumber)
                 {
+                    // Add it to the Cmn that is going to be exported
                     if (exportCmn.AddVariable(parent[key].Name, exportCmn.Root))
                     {
+                        // Iterate through each localization contained in the game
                         foreach (char letter in exportDats.Keys)
                         {
+                            // Check if the mod contains that localization
                             if (modDats.ContainsKey(letter))
                             {
-                                string newString = modDats[letter].Strings[parent[key].StringNumber];
+                                // Check if the string exist in that localization
+                                // If it doesn't, put a null string
+                                string newString = modDats[letter].Strings.Count <= parent[key].StringNumber 
+                                    ? "\0" 
+                                    : modDats[letter].Strings[parent[key].StringNumber];
                                 exportDats[letter].Strings.Add(newString);
                             }
                             else
                             {
+                                // If it doesn't exist, add a null string
                                 exportDats[letter].Strings.Add("\0");
                             }
                         }
